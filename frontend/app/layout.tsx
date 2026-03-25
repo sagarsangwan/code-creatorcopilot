292ddc868eafade1a983746154c3153ae96be729
sagarsangwan <iamsagarsangwan@gmail.com>
2026-03-25 10:36:42 +0530

    added authjs in frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
-
+import { SessionProvider } from "next-auth/react";
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
@@ -29,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SessionProvider>
         {children}
+        </SessionProvider>
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,14 @@
+import SignIn from "@/components/auth/sign-in";
+import { SignOut } from "@/components/auth/signout-button";
 import { ComponentExample } from "@/components/component-example";
+import { auth } from "@/lib/auth";
 
-export default function Page() {
-return <ComponentExample />;
+export default async function Page() {
+    const session = await auth()
+    console.log(session?.user)
+return (
+    <div>
+        {session?.user ? <SignOut/> : <SignIn/>}
+    </div>
+)
 }
